5ea71bd3b710426fc2e01147bac5cf34f52a3e87
diego-nascimento <diegostealth3@gmail.com>
2020-04-06 17:01:20 -0300

    javascript e animações finalizadas

---
+++ js/script.js
@@ -0,0 +1,19 @@
+if(window.SimpleSlide) {
+  new SimpleSlide({
+    slide: "quote", // nome do atributo data-slide="principal"
+    auto: true, // se o slide deve passar automaticamente
+    time: 3000, // tempo de transição dos slides
+  });
+
+  new SimpleSlide({
+    slide: "lista", // nome do atributo data-slide="principal"
+    auto: true, // se o slide deve passar automaticamente
+    time: 5000, // tempo de transição dos slides
+    nav: true
+  });
+}
+
+if(window.SimpleAnime){
+  new SimpleAnime()
+}
+  
